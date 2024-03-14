a07fce9f0b2a40b0a1ecc0af8034604db6b98c0b
Robsonpython <robsonnascimento20@gmail.com>
2024-03-14 14:51:49 -0300

    Variaveis_constates.py

--- Variaveis_constates.py
+++ Variaveis_constates.py
@@ -1,2 +1,14 @@
 nome = "Robson"
 idade = 31
+
+nome = "Giovanna"
+
+print(nome, idade)
+
+limite_saque_diario = 1000
+
+BRAZILIAN_STATES = ["SP", "RJ","SC","RS"]
+
+BRAZILIAN_STATES = 10
+
+print(BRAZILIAN_STATES)
