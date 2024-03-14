26bb35235b099d2537f10bf22c7f1b11c7485e60
Robsonpython <robsonnascimento20@gmail.com>
2024-03-14 16:16:45 -0300

    funcao_input.py

---
+++ funcao_input.py
@@ -0,0 +1,17 @@
+nome = "Guilherme"
+sobrenome = "Carvalho"
+
+print(nome, sobrenome)
+print(nome, sobrenome, end="...\n")
+print(nome, sobrenome, sep="#")
+
+nome = input("informe o seu nome:")
+idade = input("informe a sua idade")
+
+
+print(nome, idade)
+print("teste",end=" ")
+print(nome, idade, end="...\n")
+print(nome, idade, sep="#", end="...\n")
+print(nome, idade, sep="#")
+
