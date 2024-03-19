68bf03af1e43162c2fa61c2c8807c37e74f5c7e3
Robsonpython <robsonnascimento20@gmail.com>
2024-03-19 20:35:41 -0300

    String_multiplas.py

---
+++ String_multiplas.py
@@ -0,0 +1,22 @@
+nome = "Robson"
+
+mensagem = f"""
+    Olá meu nome é {nome},
+Eu estou apredendo python.
+    Essa mensagem tem diferentes recuos.
+"""
+
+print(mensagem)
+
+print("""
+      =========== MENU ============
+      
+    1- Depositar
+    2- Sacar
+    3- Sair
+    
+    ================================
+    
+    Obrigado por usar nosso sistema!!!!
+"""
+)
