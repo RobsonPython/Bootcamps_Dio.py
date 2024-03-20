6e411690329c4704c6ed00e6b8a5ea0447ab7fc1
Robsonpython <robsonnascimento20@gmail.com>
2024-03-20 20:26:29 -0300

    Metodos_da_classe_lista1.py

--- Lista.py
+++ Lista.py
@@ -17,4 +17,31 @@ lista.clear()
 print(lista)    # []
 
 lista.copy()
+
 print(lista)
+
+linguagens = ["python", "js", "c"]
+
+print(linguagens)   #["python", "js", "c"]
+
+linguagens.extend(["java", "csharp"])
+
+print(linguagens)   # ["python", "js", "c", "java", "csharp"]
+
+# [].Index
+linguagens = ["python", "java", "c", "java", "csharp"]
+
+print(linguagens.index("java")) # 3
+print(linguagens.index("python"))   # 0
+
+# [].Pop
+
+linguagens  = ["python", "js", "c", "java", "csharp"]
+
+linguagens.pop() # csharp
+linguagens.pop() # java
+linguagens.pop() # c
+linguagens.pop(0) # python
+
+
+
