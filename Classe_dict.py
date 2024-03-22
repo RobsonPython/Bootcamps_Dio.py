fa6f4a54112e13ed1fd6bc167c10c7b658c63a06
Robsonpython <robsonnascimento20@gmail.com>
2024-03-22 10:27:42 -0300

    Metodos_classe_dict.py

---
+++ Classe_dict.py
@@ -0,0 +1,10 @@
+# {}.clear
+contatos = {
+    "robson@gmail.com": {"nome": "Robson", "telefone": "4444-1020"},
+    "elionai@gmail.com":{"nome": "Elionai", "telefone": "3523-6429"},
+    "rodrigo@gmail.com":{"nome": "Rodrigo", "telefone": "3854-4023"},
+    "bruno@gmail.com": {"nome": "Bruno", "telefone": "5555-2356"},
+}
+
+contatos.clear()
+contatos # {}
